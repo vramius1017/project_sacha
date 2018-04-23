57fff7b8da4cf0ca71e14ebc0adbf6df47c40724
Sacha  TORRES <vramius1017@users.noreply.github.com>
2018-04-23 17:03:01 +0200

    Create form_end.php

---
+++ form_end.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+?>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>AR</title>
+</head>
+<body>
+<form>
+<!--Champ commentaires  -->
+<p align="center">
+    <legend>Commentaires</legend>
+    <textarea name="adress"cols="40" rows="4"></textarea>
+</p>
+
+<!-- bouton envoyer   -->
+<p align="center">
+    <input type="submit" value="Envoyer"/>
+</p>
+</form>
+        <h2 align="center">Je vous remercie pour votre demande.</br>
+            Vous allez recevoir un mail accusant reception de votre demande .</br>
+            une réponse vous sera prochainement donné.
+             </h2>
+
+</body>
+</html>
