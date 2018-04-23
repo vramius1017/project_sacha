72fab4cbbcd2e7c468071e4d5b609b11220f4630
Sacha  TORRES <vramius1017@users.noreply.github.com>
2018-04-23 16:55:28 +0200

    Create query_select.php

---
+++ query_select.php
@@ -0,0 +1,31 @@
+<?php
+
+
+/*
+*
+*query_select
+*
+*
+/*
+
+function get_event ($con) {
+	/* query to select the event in form_event*/
+	$sql = '';
+  }
+  
+function get_dem($con){
+	/* query to select the event in form_end*/
+ $sql = '';   
+   }  
+  
+  
+
+
+
+
+
+
+
+
+
+?>
